43396c7a9c4037e3a132ad3ffee29f74d65f02f8
pvdstel <pvdstel@users.noreply.github.com>
2020-11-15 17:55:11 +0100

    Add extension tests

---
+++ Marionet.Core.Test/ExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Marionet.Core.Test
+{
+    public class ExtensionsTest
+    {
+        [Fact]
+        public void TestNameNormalization()
+        {
+            // The normalization is primarily used for equality checks; therefore the same is done here.
+            Assert.Equal("name".NormalizeDesktopName(), "name".NormalizeDesktopName());
+            Assert.Equal("name-1".NormalizeDesktopName(), "name-1".NormalizeDesktopName());
+            Assert.Equal("name-1".NormalizeDesktopName(), "NaME-1".NormalizeDesktopName());
+            Assert.Equal("name-1".NormalizeDesktopName(), "NAME-1".NormalizeDesktopName());
+        }
+    }
+}
