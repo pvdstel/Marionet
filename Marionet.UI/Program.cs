68452deb2e448f16c6ed9762396664eee400b8fb
pvdstel <pvdstel@users.noreply.github.com>
2020-11-19 12:50:00 +0100

    Prefix mutexes with application name

--- Marionet.UI/Program.cs
+++ Marionet.UI/Program.cs
@@ -14,8 +14,8 @@ namespace Marionet.UI
 {
     public static class Program
     {
-        private const string ShowSignalName = "7e29830a-272c-4354-85e7-1a85a0e6a48c";
-        private const string FirstMutexName = "d0ab88ed-49ac-45f1-b695-1ba3c6d23b1c";
+        private const string ShowSignalName = "Marionet:7e29830a-272c-4354-85e7-1a85a0e6a48c";
+        private const string FirstMutexName = "Marionet:d0ab88ed-49ac-45f1-b695-1ba3c6d23b1c";
 
         private static readonly CancellationTokenSource appShutdownToken = new CancellationTokenSource();
         private static readonly EventWaitHandle signal = new EventWaitHandle(false, EventResetMode.AutoReset, ShowSignalName);
